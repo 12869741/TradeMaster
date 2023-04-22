e456a0ea5341dd315fc0a330fc972619adae3814
12869741 <72336515+12869741@users.noreply.github.com>
2023-04-22 17:11:56 +0800

    Update dqn_two_actions.py

--- agent/aaai2020/dqn_two_actions.py
+++ agent/aaai2020/dqn_two_actions.py
@@ -58,7 +58,7 @@ def get_args():
     parser.add_argument('--gamma', type=float, default=1.0)
     parser.add_argument('--n-step', type=int, default=1)
     parser.add_argument('--target-update-freq', type=int, default=320)
-    parser.add_argument('--epoch', type=int, default=10)
+    parser.add_argument('--epoch', type=int, default=1)
     parser.add_argument('--step-per-epoch', type=int, default=1000)
     parser.add_argument('--collect-per-step', type=int, default=20)
     parser.add_argument('--batch-size', type=int, default=64)
@@ -66,7 +66,7 @@ def get_args():
     parser.add_argument('--training-num', type=int, default=8)
     parser.add_argument('--test-num', type=int, default=32)
     parser.add_argument('--logdir', type=str, default='log')
-    parser.add_argument('--render', type=float, default=0.)
+    parser.add_argument('--render', type=float, default=1.)
     parser.add_argument('--device',
                         type=str,
                         default='cuda' if torch.cuda.is_available() else 'cpu')
